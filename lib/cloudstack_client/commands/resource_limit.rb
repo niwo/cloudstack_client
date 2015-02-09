9bffcd5b7ed587973b09ced13fae52030b77ff72
niwo <nik.wolfgramm@gmail.com>
2015-02-09 17:56:06 +0100

    Extended ResourceLimit module

--- lib/cloudstack_client/commands/resource_limit.rb
+++ lib/cloudstack_client/commands/resource_limit.rb
@@ -1,7 +1,6 @@
 module CloudstackClient
 
   module ResourceLimit
-
     ##
     # List resource limits.
 
@@ -16,7 +15,7 @@ module CloudstackClient
           puts "Error: Account #{args[:account]} not found."
           exit 1
         end
-        params['domainid'] = account["domainid"]
+        params['domainid'] = account['domainid']
         params['account'] = args[:account]
       end
 
@@ -33,6 +32,89 @@ module CloudstackClient
       end
 
       params['type'] = args[:type] if args[:type]
+
+      json = send_request(params)
+      json['resourcelimit'] || []
+    end
+
+    ##
+    # Recalculate and update resource count for an account or domain.
+
+    def update_resource_count(args = {})
+      params = {
+        'command' => 'updateResourceCount',
+      }
+
+      if args[:account]
+        account = list_accounts({name: args[:account]}).first
+        unless account
+          puts "Error: Account #{args[:account]} not found."
+          exit 1
+        end
+        params['domainid'] = account['domainid']
+        params['account'] = args[:account]
+      end
+
+      if args[:project]
+        project = get_project(args[:project])
+        if !project
+          msg = "Project '#{args[:project]}' is invalid"
+          puts "Error: #{msg}"
+          exit 1
+        end
+        params['domainid'] = project['domainid']
+        params['projectid'] = project['id']
+      elsif args[:project_id]
+        params['projectid'] = args[:project_id]
+      end
+
+      params['resourcetype'] = args[:resource_type] if args[:resource_type]
+      params['domainid'] = args[:domain_id] if args[:domain_id]
+
+      json = send_request(params)
+      json['resourcecount'] || []
+    end
+
+    ##
+    # Updates resource limits for an account or domain.
+
+    def update_resource_limit(args = {})
+      params = {
+        'command' => 'updateResourceLimit',
+      }
+
+      if args[:resource_type]
+        params['resourcetype'] = args[:resource_type]
+      else
+        puts "Error: Resource Type must be specified."
+        exit 1
+      end
+
+      if args[:account]
+        account = list_accounts({name: args[:account]}).first
+        unless account
+          puts "Error: Account #{args[:account]} not found."
+          exit 1
+        end
+        params['domainid'] = account['domainid']
+        params['account'] = args[:account]
+      end
+
+      if args[:project]
+        project = get_project(args[:project])
+        if !project
+          msg = "Project '#{args[:project]}' is invalid"
+          puts "Error: #{msg}"
+          exit 1
+        end
+        params['domainid'] = project['domainid']
+        params['projectid'] = project['id']
+      elsif args[:project_id]
+        params['projectid'] = args[:project_id]
+      end
+
+      params['domainid'] = args[:domain_id] if args[:domain_id]
+      params['max'] = args[:max] if args[:max]
 
       json = send_request(params)
       json['resourcelimit'] || []
