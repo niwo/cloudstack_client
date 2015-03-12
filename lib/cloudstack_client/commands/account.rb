9de2d4863d1bbfa4d89c8868ac5df674da8fc03e
niwo <nik.wolfgramm@gmail.com>
2015-03-12 10:06:01 +0100

    add list_project_accounts command

--- lib/cloudstack_client/commands/account.rb
+++ lib/cloudstack_client/commands/account.rb
@@ -1,8 +1,8 @@
 module CloudstackClient
 
-	module Account
+  module Account
 
-		##
+    ##
     # Lists accounts.
 
     def list_accounts(args = { :name => nil })
@@ -17,6 +17,22 @@ module CloudstackClient
       json['account'] || []
     end
 
-	end
+    ##
+    # Lists project accounts.
+
+    def list_project_accounts(project_id, args)
+      params = {
+        'command' => 'listProjectAccounts',
+        'projectid' => project_id,
+        'listall' => 'true',
+        'isrecursive' => 'true'
+      }
+      params['name'] = args[:name] if args[:name]
+
+      json = send_request(params)
+      json['account'] || []
+    end
+
+  end
 
 end
